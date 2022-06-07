0dec0c37354a5342da2dbf1a218e2a2a0f7c2602
Nuno Pereira <nampereira@gmail.com>
2022-06-07 09:03:56 +0100

    feat!:new config settings structure

--- src/common/config.py
+++ src/common/config.py
@@ -3,24 +3,24 @@ from dynaconf import Dynaconf, Validator
 import uuid
 
 settings = Dynaconf(
-    settings_files=['settings.yaml', '.secrets.yaml'],
+    settings_files=["settings.yaml", ".secrets.yaml", ".appsettings.yaml"],
     validators=[
         # runtime required settings
-        Validator('runtime.name', 'runtime.uuid', 'runtime.runtime_type', 'runtime.apis', 'runtime.reg_attempts', 'runtime.reg_timeout_seconds', 'runtime.max_nmodules', 'runtime.realm', must_exist=True),
+        Validator("runtime.name", "runtime.uuid", "runtime.runtime_type", "runtime.apis", "runtime.reg_attempts", "runtime.reg_timeout_seconds", "runtime.max_nmodules", "runtime.realm", must_exist=True),
 
         # gen runtime uuid default value (if empty)
         Validator("runtime.uuid", default=str(uuid.uuid4())),
 
         # mqtt required settings
-        Validator('mqtt.host', 'mqtt.port', 'mqtt.ssl', must_exist=True),
+        Validator("mqtt.host", "mqtt.port", "mqtt.ssl", must_exist=True),
 
-        # topic list is required (can't check particular topics due to template substitution ?)
-        Validator('topics', must_exist=True),
+        # topic list is required (can"t check particular topics due to template substitution ?)
+        Validator("topics", must_exist=True),
 
         # repository required settings
-        Validator('repository.class', 'repository.args', must_exist=True),
+        Validator("repository.class", must_exist=True),
 
-        # laucher required settings
-        Validator('launcher.class', 'launcher.apis', 'launcher.args', must_exist=True),
+        # launcher required settings
+        Validator("launcher.apis", must_exist=True),
     ],
 )
