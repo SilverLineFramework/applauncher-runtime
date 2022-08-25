08d429b2b5b520f54a46ea09b6452f8510597ef7
Nuno Pereira <nampereira@gmail.com>
2022-08-25 10:47:43 +0100

    Fix imports

--- src/launcher/__init__.py
+++ src/launcher/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 try:
-    from .docker import *
-    from .filestore_repo import *
-    from .launcher_proto import *
+    from .docker_client import *
+    from .launcher import *
+    from .python_launcher import *
 except ImportError:
     # this might be relevant during the installation process
     pass
