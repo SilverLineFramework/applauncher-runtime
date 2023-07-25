e2e7023904745823f9005ad8ae6551442b0afe82
Nuno Pereira <nampereira@gmail.com>
2023-07-25 16:59:31 +0100

    fix: update arenaxr start config

--- start-runtime1.sh
+++ start-runtime1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screen -L -Logfile /home/wiselab/silverline/applauncher-runtime/slruntime.log -S slruntime1 -dm bash -c "make run_arenaxr"
+screen -L -Logfile /home/wiselab/silverline/applauncher-runtime/slruntime.log -S slruntime1 -dm bash -c "make config/arenaxr"
