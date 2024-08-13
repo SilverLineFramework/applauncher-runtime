691edbefc0b89bb171403f62de0de64731b2c333
Nuno Pereira <nampereira@gmail.com>
2024-08-13 12:07:28 +0100

    feat: target config fix

--- start-runtime1.sh
+++ start-runtime1.sh
@@ -2,5 +2,5 @@
 # usage ./start-runtime1 [target-config]
 # where target-config specifies the config to be used (e.g. arenaxr or dev-1); default is arenaxr
 
-TARGET="${1:-$arenaxr}"
+TARGET="${1:-arenaxr}"
 screen -L -Logfile slruntime1.log -S slruntime1 -dm bash -c "make config/${TARGET}"
