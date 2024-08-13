fd72822693aa17d7bdd3c4a7f7ea745a7e28d036
Nuno Pereira <nampereira@gmail.com>
2024-08-13 12:02:33 +0100

    feat: target config fix

--- start-runtime1.sh
+++ start-runtime1.sh
@@ -3,4 +3,4 @@
 # where target-config specifies the config to be used (e.g. arenaxr or dev-1); default is arenaxr
 
 TARGET="${1:-$arenaxr}"
-screen -L -Logfile /home/wiselab/silverline/applauncher-runtime/slruntime.log -S slruntime1 -dm bash -c "make config/${TARGET}"
+screen -L -Logfile slruntime1.log -S slruntime1 -dm bash -c "make config/${TARGET}"
