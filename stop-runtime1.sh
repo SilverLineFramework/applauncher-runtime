9e4ce7db573b52509c783aef5f9af7cd134c8b81
Nuno Pereira <nampereira@gmail.com>
2024-08-22 10:38:41 +0100

    fix: stop script wait

--- stop-runtime1.sh
+++ stop-runtime1.sh
@@ -2,8 +2,18 @@
 
 # sending 'Q' to runtime will request it to exit gracefully (cleanup containers)
 screen -S slruntime1 -X stuff 'Q'`echo -ne '\015'` 
+attempt=1
 while screen -list | grep -q slruntime1
 do
+	if [ attempt -eq 1 ]; then
+		echo "Waiting for runtime to exit gracefully"
+	fi
+	if [ $attempt -gt 20 ]; then
+        echo "Runtime did not exit after $max_attempts attempts, forcefully stopping it."
+        screen -X -S slruntime1 quit
+        break
+    fi
+    attempt=$((attempt + 1))    
     sleep 1
 done
 # still try to kill runner containers in case exit failed to cleanup correctly
