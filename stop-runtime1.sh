eda018037465486188ccc379e1e43fa54a4f5c58
Nuno Pereira <nampereira@gmail.com>
2024-08-22 10:23:25 +0100

    fix: stop script

--- stop-runtime1.sh
+++ stop-runtime1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-screen -X -S slruntime1 quit 
-#screen -ls | grep '(Detached)' | awk '{print $1}' | xargs -I % -t screen -X -S % quit
-(docker kill `docker ps | grep slruntime-python-runner | cut -d" " -f1` 2>&1) > /dev/null
+# sending 'Q' to runtime will request it to exit gracefully (cleanup containers)
+screen -S slruntime1 -X stuff 'Q'`echo -ne '\015'` 
+# still try to kill runner containers in case exit failed to cleanup correctly
+This many runners still alive (should be zero; will kill them if not): `docker ps | grep slruntime-python-runner | wc -l`
+(docker kill `docker ps | grep slruntime-python-runner | cut -d" " -f1` 2>&1) > /dev/null 
 
