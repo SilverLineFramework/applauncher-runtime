b56fef46fd4cbefde555460ac8411e59374a479b
Nuno Pereira <nampereira@gmail.com>
2024-11-19 22:35:51 +0000

    fix: start runtime script

--- start-runtime1.sh
+++ start-runtime1.sh
@@ -3,7 +3,7 @@
 # where target-config specifies the config to be used (e.g. arenaxr or dev-1); default is arenaxr
 
 docker pull slframework/slruntime-python-runner-repo-head
-docker pull slframework/slruntime-python
+docker pull slframework/slruntime-python-runner
 
 TARGET="${1:-arenaxr}"
 screen -L -Logfile slruntime1.log -S slruntime1 -dm bash -c "make config/${TARGET}"
